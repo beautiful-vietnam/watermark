04ff7e383636cd6aa55c9314d995d90c9db93f12
thaycacac <thaycacac@gmail.com>
2019-04-25 21:35:22 +0700

    done: watermark

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,33 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
+  <v-layout>
+    <v-flex text-xs-center>
+      <v-btn color="danger" @click="$refs.inputUpload.click()">Add Logo</v-btn>
+      <input v-show="false" ref="inputUpload" type="file" @change="convert" />
+      <div id="container">
+        <img src="/logo1.png" alt="logo" />
       </div>
-      <v-card>
-        <v-card-title class="headline"
-          >Welcome to the Vuetify + Nuxt.js template</v-card-title
-        >
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-              >issue board</a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
     </v-flex>
   </v-layout>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import watermark from 'watermarkjs'
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  methods: {
+    convert() {
+      const upload = document.querySelector('input[type=file]').files[0]
+      watermark([upload, '/logo.png'])
+        .image(watermark.image.lowerRight(0.8))
+        .then(img => {
+          const node = document.getElementById('container')
+          node.removeChild(node.firstChild)
+          node.appendChild(img)
+        })
+    }
   }
 }
 </script>
+<style>
+img {
+  max-width: 100%;
+}
+</style>
